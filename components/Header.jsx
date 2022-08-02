8740e0344e230a9cb970c6ec9b4905e5877b26e3
mubarizmayilzade <mubarizbm@code.edu.az>
2022-08-02 23:59:37 +0400

    header structure

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <p>About</p>
+        <p>Store</p>
+      </div>
+      <div>
+
+      </div>
+    </header>
+  )
+}
