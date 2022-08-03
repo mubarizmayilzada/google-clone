69b55f4e49766fcf44c0f245ae7d89cfc2e6aafd
mubarizmayilzade <mubarizbm@code.edu.az>
2022-08-03 05:28:24 +0400

    configured next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
     // ...add more providers here
   ],
   secret:process.env.SECRET,
